fb58015cd615937839e3f1e1ed489614c00362b3
Fors1kGato <68744677+Fors1kGato@users.noreply.github.com>
2021-05-29 15:32:08 +0300

    Update Get-IniContent.ps1

--- Get-IniContent.ps1
+++ Get-IniContent.ps1
@@ -1,8 +1,6 @@
-function Get-IniContent ($filePath)
-{
+ function Get-IniContent ($filePath){
     $ini = @{}
-    switch -regex -file $FilePath
-    {
+    switch -regex -file $FilePath{
         '^\[(.+)\]' # Section
         {
             $section = $matches[1]
@@ -24,3 +22,26 @@ function Get-IniContent ($filePath)
     }
     return $ini
 }
+function Out-IniFile($InputObject, $FilePath){
+    $outFile = New-Item -ItemType file -Path $Filepath -Force
+    foreach ($i in $InputObject.keys){
+        if (!($($InputObject[$i].GetType().Name) -eq "Hashtable")){
+            #No Sections
+            Add-Content -Path $outFile -Value "$i=$($InputObject[$i])"
+        }
+        else{
+            #Sections
+            Add-Content -Path $outFile -Value "[$i]"
+            Foreach ($j in ($InputObject[$i].keys | Sort-Object)){
+                if ($j -match "^Comment[\d]+") {
+                    Add-Content -Path $outFile -Value "$($InputObject[$i][$j])"
+                }
+                else {
+                    Add-Content -Path $outFile -Value "$j=$($InputObject[$i][$j])"
+                }
+
+            }
+            Add-Content -Path $outFile -Value ""
+        }
+    }
+} 
